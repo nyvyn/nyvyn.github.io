447439f3cfc46036edafee53e1c2932c37d777be
nyvyn <2886602+nyvyn@users.noreply.github.com>
2025-01-12 05:41:24 -0600

    refactor: Rewrite worker.js to use vanilla JavaScript without imports

--- assets/js/worker.js
+++ assets/js/worker.js
@@ -1,93 +1,93 @@
-import { pipeline } from 'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.2.4';
-import {
-    AutoTokenizer,
-    AutoModelForCausalLM,
-    TextStreamer,
-    InterruptableStoppingCriteria,
-} from "@huggingface/transformers";
-
 /**
- * This class uses the Singleton pattern to enable lazy-loading of the pipeline
+ * This script is a vanilla JavaScript version of the worker.js file.
+ * It assumes that the necessary libraries are loaded via script tags in the HTML.
  */
-class TextGenerationPipeline {
-    static model_id = "onnx-community/Phi-3.5-mini-instruct-onnx-web";
 
-    static async getInstance(progress_callback = null) {
-        this.tokenizer ??= AutoTokenizer.from_pretrained(this.model_id, {
-            progress_callback,
+// Singleton pattern for lazy-loading the pipeline
+var TextGenerationPipeline = (function () {
+    var instance;
+    var model_id = "onnx-community/Phi-3.5-mini-instruct-onnx-web";
+    var tokenizer, model;
+
+    async function init(progress_callback) {
+        tokenizer = await AutoTokenizer.from_pretrained(model_id, {
+            progress_callback: progress_callback,
         });
 
-        this.model ??= AutoModelForCausalLM.from_pretrained(this.model_id, {
-            // dtype: "q4",
+        model = await AutoModelForCausalLM.from_pretrained(model_id, {
             dtype: "q4f16",
             device: "webgpu",
             use_external_data_format: true,
-            progress_callback,
+            progress_callback: progress_callback,
         });
 
-        return Promise.all([this.tokenizer, this.model]);
+        return [tokenizer, model];
     }
-}
 
-const stopping_criteria = new InterruptableStoppingCriteria();
+    return {
+        getInstance: async function (progress_callback) {
+            if (!instance) {
+                instance = init(progress_callback);
+            }
+            return instance;
+        },
+    };
+})();
 
-let past_key_values_cache = null;
+var stopping_criteria = new InterruptableStoppingCriteria();
+var past_key_values_cache = null;
+
 async function generate(messages) {
-    // Retrieve the text-generation pipeline.
-    const [tokenizer, model] = await TextGenerationPipeline.getInstance();
+    var [tokenizer, model] = await TextGenerationPipeline.getInstance();
 
-    const inputs = tokenizer.apply_chat_template(messages, {
+    var inputs = tokenizer.apply_chat_template(messages, {
         add_generation_prompt: true,
         return_dict: true,
     });
 
-    let startTime;
-    let numTokens = 0;
-    let tps;
-    const token_callback_function = () => {
-        startTime ??= performance.now();
+    var startTime;
+    var numTokens = 0;
+    var tps;
+    var token_callback_function = function () {
+        startTime = startTime || performance.now();
 
         if (numTokens++ > 0) {
             tps = (numTokens / (performance.now() - startTime)) * 1000;
         }
     };
-    const callback_function = (output) => {
+    var callback_function = function (output) {
         self.postMessage({
             status: "update",
-            output,
-            tps,
-            numTokens,
+            output: output,
+            tps: tps,
+            numTokens: numTokens,
         });
     };
 
-    const streamer = new TextStreamer(tokenizer, {
+    var streamer = new TextStreamer(tokenizer, {
         skip_prompt: true,
         skip_special_tokens: true,
-        callback_function,
-        token_callback_function,
+        callback_function: callback_function,
+        token_callback_function: token_callback_function,
     });
 
     // Tell the main thread we are starting
     self.postMessage({ status: "start" });
 
-    const { past_key_values, sequences } = await model.generate({
+    var result = await model.generate({
         ...inputs,
-        // TODO: Enable once model is fixed
-        // past_key_values: past_key_values_cache,
-
-        // Sampling
         do_sample: true,
         top_k: 3,
         temperature: 0.2,
-
         max_new_tokens: 1024,
-        streamer,
-        stopping_criteria,
+        streamer: streamer,
+        stopping_criteria: stopping_criteria,
         return_dict_in_generate: true,
     });
-    past_key_values_cache = past_key_values;
 
-    const decoded = tokenizer.batch_decode(sequences, {
+    past_key_values_cache = result.past_key_values;
+
+    var decoded = tokenizer.batch_decode(result.sequences, {
         skip_special_tokens: true,
     });
 
@@ -100,7 +100,7 @@ async function generate(messages) {
 
 async function check() {
     try {
-        const adapter = await navigator.gpu.requestAdapter();
+        var adapter = await navigator.gpu.requestAdapter();
         if (!adapter) {
             throw new Error("WebGPU is not supported (no adapter found)");
         }
@@ -119,9 +119,7 @@ async function load() {
     });
 
     // Load the pipeline and save it for future use.
-    const [tokenizer, model] = await TextGenerationPipeline.getInstance((x) => {
-        // We also add a progress callback to the pipeline so that we can
-        // track model loading.
+    var [tokenizer, model] = await TextGenerationPipeline.getInstance(function (x) {
         self.postMessage(x);
     });
 
@@ -131,13 +129,15 @@ async function load() {
     });
 
     // Run model with dummy input to compile shaders
-    const inputs = tokenizer("a");
+    var inputs = tokenizer("a");
     await model.generate({ ...inputs, max_new_tokens: 1 });
     self.postMessage({ status: "ready" });
 }
+
 // Listen for messages from the main thread
-self.addEventListener("message", async (e) => {
-    const { type, data } = e.data;
+self.addEventListener("message", async function (e) {
+    var type = e.data.type;
+    var data = e.data.data;
 
     switch (type) {
         case "check":
